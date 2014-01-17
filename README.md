55fd02a2c3dfbc36486d5c3257471591f5d5b5e1
Asaf David <asafdav@gmail.com>
2014-01-18 01:28:41 +0200

    First version

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-ng-scrollbar
-============
+# ng-scrollbar
+A custom scrollbar written in pure AngularJS
 
-A custom scrollbar written in pure AngularJS 
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/asafdav/jquery-ng-scrollbar/master/dist/angular-ng-scrollbar.min.js
+[max]: https://raw.github.com/asafdav/jquery-ng-scrollbar/master/dist/angular-ng-scrollbar.js
+
+In your web page:
+
+```html
+<script src="angular.js"></script>
+<script src="dist/ng-scrollbar.min.js"></script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
---
+++ dist/ng-scrollbar.js
@@ -0,0 +1,120 @@
+'use strict';
+angular.module('ngScrollbar', []).directive('ngScrollbar', [
+  '$parse',
+  '$window',
+  function ($parse, $window) {
+    return {
+      restrict: 'A',
+      replace: true,
+      transclude: true,
+      link: function (scope, element, attrs) {
+        var win, mainElm, transculdedContainer, tools, thumb, thumbLine, track;
+        var dragger = { top: 0 }, page = { top: 0 };
+        var scrollboxStyle, draggerStyle, draggerLineStyle, pageStyle;
+        var calcStyles = function () {
+          scrollboxStyle = {
+            position: 'relative',
+            overflow: 'hidden',
+            'max-width': '100%',
+            height: '100%'
+          };
+          if (page.height) {
+            scrollboxStyle.height = page.height + 'px';
+          }
+          draggerStyle = {
+            position: 'absolute',
+            height: dragger.height + 'px',
+            top: dragger.top + 'px'
+          };
+          draggerLineStyle = {
+            position: 'relative',
+            'line-height': dragger.height + 'px'
+          };
+          pageStyle = {
+            position: 'relative',
+            top: page.top + 'px',
+            overflow: 'hidden'
+          };
+        };
+        var redraw = function () {
+          thumb.css('top', dragger.top + 'px');
+          var draggerOffset = dragger.top / page.height;
+          page.top = -Math.round(page.scrollHeight * draggerOffset);
+          transculdedContainer.css('top', page.top + 'px');
+        };
+        var trackClick = function (event) {
+          var offsetY = event.hasOwnProperty('offsetY') ? event.offsetY : event.layerY;
+          var newTop = Math.max(0, Math.min(parseInt(dragger.trackHeight, 10) - parseInt(dragger.height, 10), offsetY));
+          dragger.top = newTop;
+          redraw();
+          event.stopPropagation();
+        };
+        var wheelHandler = function (event) {
+          var deltaY = event.wheelDeltaY !== undefined ? event.wheelDeltaY / 20 : event.wheelDelta !== undefined ? event.wheelDelta / 20 : -event.detail * 2;
+          dragger.top = Math.max(0, Math.min(parseInt(page.height, 10) - parseInt(dragger.height, 10), parseInt(dragger.top, 10) - deltaY));
+          redraw();
+          if (!!event.preventDefault) {
+            event.preventDefault();
+          } else {
+            return false;
+          }
+        };
+        var lastOffsetY;
+        var thumbDrag = function (event, offsetX, offsetY) {
+          var newTop = Math.max(0, Math.min(parseInt(dragger.trackHeight, 10) - parseInt(dragger.height, 10), offsetY));
+          dragger.top = newTop;
+          event.stopPropagation();
+        };
+        var dragHandler = function (event) {
+          var newOffsetY = event.pageY - thumb[0].scrollTop - lastOffsetY;
+          var newOffsetX = 0;
+          thumbDrag(event, newOffsetX, newOffsetY);
+          redraw();
+        };
+        var buildScrollbar = function () {
+          win = angular.element($window);
+          mainElm = angular.element(element.children()[0]);
+          transculdedContainer = angular.element(mainElm.children()[0]);
+          tools = angular.element(mainElm.children()[1]);
+          thumb = angular.element(angular.element(tools.children()[0]).children()[0]);
+          thumbLine = angular.element(thumb.children()[0]);
+          track = angular.element(angular.element(tools.children()[0]).children()[1]);
+          page.height = element[0].offsetHeight;
+          page.scrollHeight = transculdedContainer[0].scrollHeight;
+          if (page.height < page.scrollHeight) {
+            scope.showYScrollbar = true;
+            dragger.height = Math.round(page.height / page.scrollHeight * page.height);
+            dragger.trackHeight = page.height;
+            calcStyles();
+            element.css({ overflow: 'hidden' });
+            mainElm.css(scrollboxStyle);
+            transculdedContainer.css(pageStyle);
+            thumb.css(draggerStyle);
+            thumbLine.css(draggerLineStyle);
+            track.bind('click', trackClick);
+            var wheelEvent = win[0].onmousewheel !== undefined ? 'mousewheel' : 'DOMMouseScroll';
+            transculdedContainer.bind(wheelEvent, wheelHandler);
+            thumb.bind('mousedown', function (event) {
+              lastOffsetY = event.pageY - thumb[0].offsetTop;
+              win.bind('mouseup', function () {
+                win.unbind('mousemove', dragHandler);
+                event.stopPropagation();
+              });
+              win.bind('mousemove', dragHandler);
+            });
+          } else {
+            scope.showYScrollbar = false;
+          }
+        };
+        buildScrollbar();
+        if (!!attrs.rebuildOn) {
+          scope.$on(attrs.rebuildOn, function () {
+            page.height = null;
+            buildScrollbar();
+          });
+        }
+      },
+      template: '<div>' + '<div class="ngsb-wrap">' + '<div class="ngsb-container" ng-transclude></div>' + '<div class="ngsb-scrollbar" style="position: absolute; display: block;" ng-show="showYScrollbar">' + '<div class="ngsb-thumb-container">' + '<div class="ngsb-thumb-pos" oncontextmenu="return false;">' + '<div class="ngsb-thumb" ></div>' + '</div>' + '<div class="ngsb-track"></div>' + '</div>' + '</div>' + '</div>' + '</div>'
+    };
+  }
+]);
