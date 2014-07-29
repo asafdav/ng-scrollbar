3f9161eda48ec5ccbf9a65855b59fb2ff04b8be3
OpherV <opherv@gmail.com>
2014-07-29 11:13:38 +0300

    Fix codestyle according to lint

--- dist/ng-scrollbar.js
+++ dist/ng-scrollbar.js
@@ -8,10 +8,12 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
       replace: true,
       transclude: true,
       link: function (scope, element, attrs) {
-        var mainElm, transculdedContainer, tools, thumb, thumbLine, track;
+        var mainElm, transculdedContainer, thumb, thumbContainer, track;
         var flags = { bottom: attrs.hasOwnProperty('bottom') };
         var win = angular.element($window);
+        // Elements
         var dragger = { top: 0 }, page = { top: 0 };
+        // Styles
         var scrollboxStyle, draggerStyle, draggerLineStyle, pageStyle;
         var calcStyles = function () {
           scrollboxStyle = {
@@ -53,6 +55,7 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
         };
         var wheelHandler = function (event) {
           var wheelDivider = 20;
+          // so it can be changed easily
           var deltaY = event.wheelDeltaY !== undefined ? event.wheelDeltaY / wheelDivider : event.wheelDelta !== undefined ? event.wheelDelta / wheelDivider : -event.detail * (wheelDivider / 10);
           dragger.top = Math.max(0, Math.min(parseInt(page.height, 10) - parseInt(dragger.height, 10), parseInt(dragger.top, 10) - deltaY));
           redraw();
@@ -70,33 +73,40 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
         var dragHandler = function (event) {
           var newOffsetY = event.pageY - thumb[0].scrollTop - lastOffsetY;
           var newOffsetX = 0;
+          // TBD
           thumbDrag(event, newOffsetX, newOffsetY);
           redraw();
         };
         var buildScrollbar = function (rollToBottom) {
+          // Getting top position of a parent element to place scroll correctly
           var parentOffsetTop = element[0].parentElement.offsetTop;
           var wheelEvent = win[0].onmousewheel !== undefined ? 'mousewheel' : 'DOMMouseScroll';
           rollToBottom = flags.bottom || rollToBottom;
           mainElm = angular.element(element.children()[0]);
           transculdedContainer = angular.element(mainElm.children()[0]);
-          tools = angular.element(mainElm.children()[1]);
-          thumb = angular.element(angular.element(tools.children()[0]).children()[0]);
-          thumbLine = angular.element(thumb.children()[0]);
-          track = angular.element(angular.element(tools.children()[0]).children()[1]);
+          thumb = angular.element(element[0].querySelector('.ngsb-thumb-pos'));
+          thumbContainer = angular.element(element[0].querySelector('.ngsb-thumb'));
+          track = angular.element(element[0].querySelector('.ngsb-track'));
+          // Check if scroll bar is needed
           page.height = element[0].offsetHeight - parentOffsetTop;
           page.scrollHeight = transculdedContainer[0].scrollHeight;
           if (page.height < page.scrollHeight) {
             scope.showYScrollbar = true;
+            // Calculate the dragger height
             dragger.height = Math.round(page.height / page.scrollHeight * page.height);
             dragger.trackHeight = page.height;
+            // update the transcluded content style and clear the parent's
             calcStyles();
             element.css({ overflow: 'hidden' });
             mainElm.css(scrollboxStyle);
             transculdedContainer.css(pageStyle);
             thumb.css(draggerStyle);
-            thumbLine.css(draggerLineStyle);
+            thumbContainer.css(draggerLineStyle);
+            // Bind scroll bar events
             track.bind('click', trackClick);
+            // Handle mousewheel
             transculdedContainer[0].addEventListener(wheelEvent, wheelHandler, false);
+            // Drag the scroller
             thumb.on('mousedown', function (event) {
               lastOffsetY = event.pageY - thumb[0].offsetTop;
               win.on('mouseup', function () {
@@ -118,14 +128,17 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
             thumb.off('mousedown');
             transculdedContainer[0].removeEventListener(wheelEvent, wheelHandler, false);
             transculdedContainer.attr('style', 'position:relative;top:0');
+            // little hack to remove other inline styles
             mainElm.css({ height: '100%' });
           }
         };
         var rebuildTimer;
         var rebuild = function (e, data) {
+          /* jshint -W116 */
           if (rebuildTimer != null) {
             clearTimeout(rebuildTimer);
           }
+          /* jshint +W116 */
           var rollToBottom = !!data && !!data.rollToBottom;
           rebuildTimer = setTimeout(function () {
             page.height = null;
