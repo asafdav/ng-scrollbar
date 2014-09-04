00f01a89464b070ca00e8a4897e7c66c2c7bbcf4
csablons <csablons@megalo-company.com>
2014-09-04 17:02:21 +0200

    Work with touch screen. Better closure for the listener. Patch some case where scrollbar seem doesn't work with wrong caldul of the page height.

--- dist/ng-scrollbar.js
+++ dist/ng-scrollbar.js
@@ -11,7 +11,9 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
         var mainElm, transculdedContainer, tools, thumb, thumbLine, track;
         var flags = { bottom: attrs.hasOwnProperty('bottom') };
         var win = angular.element($window);
+        // Elements
         var dragger = { top: 0 }, page = { top: 0 };
+        // Styles
         var scrollboxStyle, draggerStyle, draggerLineStyle, pageStyle;
         var calcStyles = function () {
           scrollboxStyle = {
@@ -53,6 +55,7 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
         };
         var wheelHandler = function (event) {
           var wheelDivider = 20;
+          // so it can be changed easily
           var deltaY = event.wheelDeltaY !== undefined ? event.wheelDeltaY / wheelDivider : event.wheelDelta !== undefined ? event.wheelDelta / wheelDivider : -event.detail * (wheelDivider / 10);
           dragger.top = Math.max(0, Math.min(parseInt(page.height, 10) - parseInt(dragger.height, 10), parseInt(dragger.top, 10) - deltaY));
           redraw();
@@ -62,18 +65,35 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
             return false;
           }
         };
-        var lastOffsetY;
+        var lastOffsetY = 0;
         var thumbDrag = function (event, offsetX, offsetY) {
           dragger.top = Math.max(0, Math.min(parseInt(dragger.trackHeight, 10) - parseInt(dragger.height, 10), offsetY));
           event.stopPropagation();
         };
         var dragHandler = function (event) {
-          var newOffsetY = event.pageY - thumb[0].scrollTop - lastOffsetY;
           var newOffsetX = 0;
+          var newOffsetY = event.pageY - thumb[0].scrollTop - lastOffsetY;
           thumbDrag(event, newOffsetX, newOffsetY);
           redraw();
         };
+        var _mouseUp = function (event) {
+          win.off('mousemove', dragHandler);
+          win.off('mouseup', _mouseUp);
+          event.stopPropagation();
+        };
+        var _touchDragHandler = function (event) {
+          var newOffsetX = 0;
+          var newOffsetY = event.originalEvent.changedTouches[0].pageY - thumb[0].scrollTop - lastOffsetY;
+          thumbDrag(event, newOffsetX, newOffsetY);
+          redraw();
+        };
+        var _touchEnd = function (event) {
+          win.off('touchmove', _touchDragHandler);
+          win.off('touchend', _touchEnd);
+          event.stopPropagation();
+        };
         var buildScrollbar = function (rollToBottom) {
+          // Getting top position of a parent element to place scroll correctly
           var parentOffsetTop = element[0].parentElement.offsetTop;
           var wheelEvent = win[0].onmousewheel !== undefined ? 'mousewheel' : 'DOMMouseScroll';
           rollToBottom = flags.bottom || rollToBottom;
@@ -83,27 +103,40 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
           thumb = angular.element(angular.element(tools.children()[0]).children()[0]);
           thumbLine = angular.element(thumb.children()[0]);
           track = angular.element(angular.element(tools.children()[0]).children()[1]);
+          // Check if scroll bar is needed
           page.height = element[0].offsetHeight - parentOffsetTop;
+          if (page.height < 0) {
+            page.height = element[0].offsetHeight;
+          }
           page.scrollHeight = transculdedContainer[0].scrollHeight;
           if (page.height < page.scrollHeight) {
+            // Calculate the dragger height
             scope.showYScrollbar = true;
             dragger.height = Math.round(page.height / page.scrollHeight * page.height);
             dragger.trackHeight = page.height;
+            // update the transcluded content style and clear the parent's
             calcStyles();
             element.css({ overflow: 'hidden' });
             mainElm.css(scrollboxStyle);
             transculdedContainer.css(pageStyle);
             thumb.css(draggerStyle);
             thumbLine.css(draggerLineStyle);
+            // Bind scroll bar events
             track.bind('click', trackClick);
+            // Handl mousewheel
             transculdedContainer[0].addEventListener(wheelEvent, wheelHandler, false);
+            // Drag the scroller with the mouse
             thumb.on('mousedown', function (event) {
               lastOffsetY = event.pageY - thumb[0].offsetTop;
-              win.on('mouseup', function () {
-                win.off('mousemove', dragHandler);
-                event.stopPropagation();
-              });
+              win.on('mouseup', _mouseUp);
               win.on('mousemove', dragHandler);
+              event.preventDefault();
+            });
+            // Drag the scroller by touch
+            thumb.on('touchstart', function (event) {
+              lastOffsetY = event.originalEvent.changedTouches[0].pageY - thumb[0].offsetTop;
+              win.on('touchend', _touchEnd);
+              win.on('touchmove', _touchDragHandler);
               event.preventDefault();
             });
             if (rollToBottom) {
@@ -118,14 +151,17 @@ angular.module('ngScrollbar', []).directive('ngScrollbar', [
             thumb.off('mousedown');
             transculdedContainer[0].removeEventListener(wheelEvent, wheelHandler, false);
             transculdedContainer.attr('style', 'position:relative;top:0');
+            // little hack to remove other inline styles
             mainElm.css({ height: '100%' });
           }
         };
         var rebuildTimer;
         var rebuild = function (e, data) {
+          /* jshint -W116 */
           if (rebuildTimer != null) {
             clearTimeout(rebuildTimer);
           }
+          /* jshint +W116 */
           var rollToBottom = !!data && !!data.rollToBottom;
           rebuildTimer = setTimeout(function () {
             page.height = null;
